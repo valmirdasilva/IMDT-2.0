20b783c932057a9a11de33ee4c8a965aa4a3655d
Valmir silva <valmirdasilva1999@gmail.com>
2021-04-01 16:08:47 -0300

    Servicos home

--- includes/nav.php
+++ includes/nav.php
@@ -1,20 +1,25 @@
-<nav role="navigation" class="navbar navbar-white navbar-embossed navbar-lg navbar-fixed-top">
+<nav role="navigation" class="navbar navbar-white navbar-embossed navbar-lg">
+	<div class="text-right box-social">
+		<i class="space-icons fa fa-facebook"></i>
+		<i class="space-icons fa fa-twitter"></i>
+		<i class="space-icons fa fa-envelope-o"></i> <span>contato@imdt.com.br</span>
+	</div>
 	<div class="container">
 		<div class="navbar-header">
 			<button data-target="#navbar-collapse-02" data-toggle="collapse" class="navbar-toggle" type="button">
-			<i class="fa fa-bars"></i>
-			<span class="sr-only">Toggle navigation</span>
+				<i class="fa fa-bars"></i>
+				<span class="sr-only">Toggle navigation</span>
 			</button>
-			<a href="index.php" class="navbar-brand brand"> SCORILO </a>
+			<a href="index.php" class="navbar-brand brand"> <img class="img-fluid" src="../img-ex/imdt.png" alt=""> </a>
 		</div>
 		<div id="navbar-collapse-02" class="collapse navbar-collapse">
 			<ul class="nav navbar-nav navbar-right">
 				<li class="propClone"><a href="index.php">Home</a></li>
-				<li class="propClone"><a href="shop.php">Shop</a></li>
-				<li class="propClone"><a href="product.php">Product</a></li>
 				<li class="propClone"><a href="checkout.php">Checkout</a></li>
-				<li class="propClone"><a href="contato.php">Contact</a></li>
+				<li class="propClone"><a href="shop.php">Clientes</a></li>
+				<li class="propClone"><a href="product.php">Cases</a></li>
+				<li class="propClone"><a href="contato.php">Contato</a></li>
 			</ul>
 		</div>
 	</div>
-	</nav>
+</nav>
--- index.php
+++ index.php
@@ -1,295 +1,304 @@
 <!DOCTYPE html>
 <html>
+
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="generator" content="">
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
-<link href="css/style.css" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Dosis:200,300,400,500,600,700" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Roboto:200,300,400,500,600,700" rel="stylesheet">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="generator" content="">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+	<link href="css/style.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Dosis:200,300,400,500,600,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto:200,300,400,500,600,700" rel="stylesheet">
+	
+	<link rel="stylesheet" href="./css/style-edit.css">
 </head>
+
 <body>
 
 
-<!-- HEADER =============================-->
-<header class="item header margin-top-0">
-<div class="wrapper">
+	<!-- HEADER =============================-->
+	<header class="item header margin-top-0">
+		<div class="wrapper">
 
-<?php include './includes/nav.php';?>
+			<?php include './includes/nav.php'; ?>
 
-	<div class="container">
-		<div class="row">
-			<div class="col-md-12 text-center">
-				<div class="text-homeimage">
-					<div class="maintext-image" data-scrollreveal="enter top over 1.5s after 0.1s">
-						 Increase Digital Sales
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 text-center">
+						<div class="text-homeimage">
+							<div class="maintext-image" data-scrollreveal="enter top over 1.5s after 0.1s">
+								<h1>OLÁ, NÓS SOMOS A IMDT.</h1>
+							</div>
+							<div class="subtext-image" data-scrollreveal="enter bottom over 1.7s after 0.3s">
+							<p> Um grupo de pessoas apaixonadas pela internet e seu mundo sem fronteiras.
+								Queremos ajudar a resolver o seu problema, usando nosso conhecimento e criatividade.
+								Negócios Inteligentes atraem Clientes Inteligentes.
+								iMDT - Certificada MPS.BR.</p>	
+							</div>
+						</div>
 					</div>
-					<div class="subtext-image" data-scrollreveal="enter bottom over 1.7s after 0.3s">
-						 Boost revenue with Scorilo
+				</div>
+			</div>
+		</div>
+	</header>
+
+
+	<!-- STEPS =============================-->
+	<div class="item content">
+		<div class="container toparea">
+			<div class="row text-center">
+				<div class="col-md-4">
+					<div class="col editContent">
+						<span class="numberstep"><img src="./img-ex/caracteristica.png" alt=""></span>
+						<h3 class="numbertext">Sistemas Customizados - DRE / BI / / GetNet</h3>
+						<p>
+							PARA ENSINO A DISTÂNCIA E REUNIÕES
+						</p>
+					</div>
+					<!-- /.col-md-4 -->
+				</div>
+				<!-- /.col-md-4 col -->
+				<div class="col-md-4 editContent">
+					<div class="col">
+						<span class="numberstep"><img class="img-fluid" src="./img-ex/tela-do-celular-com-sinais-de-codigo.png" alt=""></i></span>
+						<h3 class="numbertext">Aplicativos mobile</h3>
+						<p>
+							nós ajudamos pessoas a expandirem seus mercados
+						</p>
+					</div>
+					<!-- /.col -->
+				</div>
+				<!-- /.col-md-4 col -->
+				<div class="col-md-4 editContent">
+					<div class="col">
+						<span class="numberstep"><img src="./img-ex/janela.png" alt=""></i></span>
+						<h3 class="numbertext">Sistemas de conferência web</h3>
+						<p>
+							Para ensino a distância e reuniões.
+						</p>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
-</header>
 
 
-<!-- STEPS =============================-->
-<div class="item content">
-	<div class="container toparea">
-		<div class="row text-center">
-			<div class="col-md-4">
-				<div class="col editContent">
-					<span class="numberstep"><i class="fa fa-shopping-cart"></i></span>
-					<h3 class="numbertext">Choose our Products</h3>
-					<p>
-						 Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nullam quis risus eget urna mollis ornare vel eu leo. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-					</p>
-				</div>
-				<!-- /.col-md-4 -->
-			</div>
-			<!-- /.col-md-4 col -->
-			<div class="col-md-4 editContent">
-				<div class="col">
-					<span class="numberstep"><i class="fa fa-gift"></i></span>
-					<h3 class="numbertext">Pay with PayPal or Card</h3>
-					<p>
-						 Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nullam quis risus eget urna mollis ornare vel eu leo. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-					</p>
-				</div>
-				<!-- /.col -->
-			</div>
-			<!-- /.col-md-4 col -->
-			<div class="col-md-4 editContent">
-				<div class="col">
-					<span class="numberstep"><i class="fa fa-download"></i></span>
-					<h3 class="numbertext">Get Instand Download</h3>
-					<p>
-						 Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nullam quis risus eget urna mollis ornare vel eu leo. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-					</p>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-	
-	
 	<!-- LATEST ITEMS =============================-->
-<section class="item content">
-	<div class="container">
-		<div class="underlined-title">
-			<div class="editContent">
-				<h1 class="text-center latestitems">LATEST ITEMS</h1>
+	<section class="item content">
+		<div class="container">
+			<div class="underlined-title">
+				<div class="editContent">
+					<h1 class="text-center latestitems">O que podemos fazer por você?</h1>
+				</div>
+				<div class="wow-hr type_short">
+					<span class="wow-hr-h">
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+						<i class="fa fa-star"></i>
+					</span>
+				</div>
 			</div>
-			<div class="wow-hr type_short">
-				<span class="wow-hr-h">
-				<i class="fa fa-star"></i>
-				<i class="fa fa-star"></i>
-				<i class="fa fa-star"></i>
-				</span>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-4">
-				<div class="productbox">
-					<div class="fadeshop">
-						<div class="captionshop text-center" style="display: none;">
-							<h3>Item Name</h3>
-							<p>
-								 This is a short excerpt to generally describe what the item is about.
-							</p>
-							<p>
-								<a href="#" class="learn-more detailslearn"><i class="fa fa-shopping-cart"></i> Purchase</a>
-								<a href="#" class="learn-more detailslearn"><i class="fa fa-link"></i> Details</a>
-							</p>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="productbox-home">
+						<div class="fadeshop">
+							<div class="captionshop text-center" style="display: none;">
+								<ul>
+									<li>Software</li>
+									<li>Aplicativo Mobile</li>
+									<li>Infraestrutura</li>
+									<li>Publicidade | Marketing</li>
+								</ul>
+								<p>
+									<a href="#" class="learn-more detailslearn"><i class="fa fa-link"></i> Veja mais</a>
+								</p>
+							</div>
+							<span class="maxproduct-home"><img src="./img-ex/projeto.png" alt=""></span>
+							<div class="product-details">
+							<a href="#">
+								<h1>Projetos</h1>
+							</a>
+
 						</div>
-						<span class="maxproduct"><img src="images/product1-1.jpg" alt=""></span>
-					</div>
-					<div class="product-details">
-						<a href="#">
-						<h1>Calypso Theme</h1>
-						</a>
-						<span class="price">
-						<span class="edd_price">$49.00</span>
-						</span>
-					</div>
-				</div>
-			</div>
-			<!-- /.productbox -->
-			<div class="col-md-4">
-				<div class="productbox">
-					<div class="fadeshop">
-						<div class="captionshop text-center" style="display: none;">
-							<h3>Item Name</h3>
-							<p>
-								 This is a short excerpt to generally describe what the item is about.
-							</p>
-							<p>
-								<a href="#" class="learn-more detailslearn"><i class="fa fa-shopping-cart"></i> Purchase</a>
-								<a href="#" class="learn-more detailslearn"><i class="fa fa-link"></i> Details</a>
-							</p>
 						</div>
-						<span class="maxproduct"><img src="images/product2.jpg" alt=""></span>
-					</div>
-					<div class="product-details">
-						<a href="#">
-						<h1>FastSell Theme</h1>
-						</a>
-						<span class="price">
-						<span class="edd_price">$49.00</span>
-						</span>
+
 					</div>
 				</div>
-			</div>
-			<!-- /.productbox -->
-			<div class="col-md-4">
-				<div class="productbox">
-					<div class="fadeshop">
-						<div class="captionshop text-center" style="display: none;">
-							<h3>Item Name</h3>
-							<p>
-								 This is a short excerpt to generally describe what the item is about.
-							</p>
-							<p>
-								<a href="#" class="learn-more detailslearn"><i class="fa fa-shopping-cart"></i> Purchase</a>
-								<a href="#" class="learn-more detailslearn"><i class="fa fa-link"></i> Details</a>
-							</p>
+
+				<div class="col-md-4">
+					<div class="productbox-home">
+						<div class="fadeshop">
+							<div class="captionshop text-center" style="display: none;">
+								<ul>
+									<li>Processo certificado</li>
+									<li>Escopo fechado</li>
+									<li>Profissionais qualificados</li>
+									<li>Planejamento detalhado</li>
+									<li>Opção de entregas parciais</li>
+								</ul>
+								<p>
+									<a href="#" class="learn-more detailslearn"><i class="fa fa-link"></i> Veja mais</a>
+								</p>
+							</div>
+							<span class="maxproduct-home"><img src="./img-ex/projeto.png" alt=""></span>
+							<div class="product-details">
+							<a href="#">
+								<h1 class="marg">Fabrica de software</h1>
+							</a>
+
 						</div>
-						<span class="maxproduct"><img src="images/product2-3.jpg" alt=""></span>
-					</div>
-					<div class="product-details">
-						<a href="#">
-						<h1>Biscaya Theme</h1>
-						</a>
-						<span class="price">
-						<span class="edd_price">$49.00</span>
-						</span>
+						</div>
+
 					</div>
 				</div>
+
+				<div class="col-md-4">
+					<div class="productbox-home">
+						<div class="fadeshop">
+							<div class="captionshop text-center" style="display: none;">
+								<ul>
+									<li>Monitoramento de infraestrutura</li>
+									<li>Suporte à infraestrutura</li>
+									<li>Suporte à sistemas</li>
+								</ul>
+								<p>
+									<a href="#" class="learn-more detailslearn"><i class="fa fa-link"></i> Veja mais</a>
+								</p>
+							</div>
+							<span class="maxproduct-home"><img src="./img-ex/projeto.png" alt=""></span>
+							<div class="product-details">
+							<a href="#">
+								<h1>Gestão de operações</h1>
+							</a>
+
+						</div>
+						</div>
+
+					</div>
+				</div>
+
 			</div>
 		</div>
-	</div>
-</div>
-</section>
-
-
-<!-- BUTTON =============================-->
-<div class="item content">
-	<div class="container text-center">
-		<a href="shop.html" class="homebrowseitems">Browse All Products
-		<div class="homebrowseitemsicon">
-			<i class="fa fa-star fa-spin"></i>
 		</div>
-		</a>
+	</section>
+
+
+	<!-- BUTTON =============================-->
+	<div class="item content">
+		<div class="container text-center">
+			<a href="shop.html" class="homebrowseitems">Browse All Products
+				<div class="homebrowseitemsicon">
+					<i class="fa fa-star fa-spin"></i>
+				</div>
+			</a>
+		</div>
 	</div>
-</div>
-<br/>
+	<br />
 
 
-<!-- AREA =============================-->
-<div class="item content">
-	<div class="container">
-		<div class="row">
-			<div class="col-md-4">
-				<i class="fa fa-microphone infoareaicon"></i>
-				<div class="infoareawrap">
-					<h1 class="text-center subtitle">General Questions</h1>
-					<p>
-						 Want to buy a theme but not sure if it's got all the features you need? Trouble completing the payment? Or just want to say hi? Send us your message and we will answer as soon as possible!
-					</p>
-					<p class="text-center">
-						<a href="#">- Get in Touch -</a>
-					</p>
+	<!-- AREA =============================-->
+	<div class="item content">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-4">
+					<i class="fa fa-microphone infoareaicon"></i>
+					<div class="infoareawrap">
+						<h1 class="text-center subtitle">General Questions</h1>
+						<p>
+							Want to buy a theme but not sure if it's got all the features you need? Trouble completing the payment? Or just want to say hi? Send us your message and we will answer as soon as possible!
+						</p>
+						<p class="text-center">
+							<a href="#">- Get in Touch -</a>
+						</p>
+					</div>
+				</div>
+				<!-- /.col-md-4 col -->
+				<div class="col-md-4">
+					<i class="fa fa-comments infoareaicon"></i>
+					<div class="infoareawrap">
+						<h1 class="text-center subtitle">Theme Support</h1>
+						<p>
+							Theme support issues prevent the theme from working as advertised in the demo. This is a free and guaranteed service offered through our support forum which is found in each theme.
+						</p>
+						<p class="text-center">
+							<a href="#">- Select Theme -</a>
+						</p>
+					</div>
+				</div>
+				<!-- /.col-md-4 col -->
+				<div class="col-md-4">
+					<i class="fa fa-bullhorn infoareaicon"></i>
+					<div class="infoareawrap">
+						<h1 class="text-center subtitle">Hire Us</h1>
+						<p>
+							If you wish to change an element to look or function differently than shown in the demo, we will be glad to assist you. This is a paid service due to theme support requests solved with priority.
+						</p>
+						<p class="text-center">
+							<a href="#">- Get in Touch -</a>
+						</p>
+					</div>
 				</div>
 			</div>
-			<!-- /.col-md-4 col -->
-			<div class="col-md-4">
-				<i class="fa fa-comments infoareaicon"></i>
-				<div class="infoareawrap">
-					<h1 class="text-center subtitle">Theme Support</h1>
-					<p>
-						 Theme support issues prevent the theme from working as advertised in the demo. This is a free and guaranteed service offered through our support forum which is found in each theme.
-					</p>
-					<p class="text-center">
-						<a href="#">- Select Theme -</a>
-					</p>
-				</div>
-			</div>
-			<!-- /.col-md-4 col -->
-			<div class="col-md-4">
-				<i class="fa fa-bullhorn infoareaicon"></i>
-				<div class="infoareawrap">
-					<h1 class="text-center subtitle">Hire Us</h1>
-					<p>
-						 If you wish to change an element to look or function differently than shown in the demo, we will be glad to assist you. This is a paid service due to theme support requests solved with priority.
-					</p>
-					<p class="text-center">
-						<a href="#">- Get in Touch -</a>
-					</p>
+		</div>
+	</div>
+
+
+	<!-- TESTIMONIAL =============================-->
+	<div class="item content">
+		<div class="container">
+			<div class="col-md-10 col-md-offset-1">
+				<div class="slide-text">
+					<div>
+						<h2><span class="uppercase">Awesome Support</span></h2>
+						<img src="http://wowthemes.net/demo/salique/salique-boxed/images/temp/avatar2.png" alt="Awesome Support">
+						<p>
+							The support... I can only say it's awesome. You make a product and you help people out any way you can even if it means that you have to log in on their dashboard to sort out any problems that customer might have. Simply Outstanding!
+						</p>
+						<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
 
-<!-- TESTIMONIAL =============================-->
-<div class="item content">
-	<div class="container">
-		<div class="col-md-10 col-md-offset-1">
-			<div class="slide-text">
-				<div>
-					<h2><span class="uppercase">Awesome Support</span></h2>
-					<img src="http://wowthemes.net/demo/salique/salique-boxed/images/temp/avatar2.png" alt="Awesome Support">
-					<p>
-						 The support... I can only say it's awesome. You make a product and you help people out any way you can even if it means that you have to log in on their dashboard to sort out any problems that customer might have. Simply Outstanding!
-					</p>
-					<i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i><i class="fa fa-star"></i>
+	<!-- CALL TO ACTION =============================-->
+	<section class="content-block" style="background-color:#00bba7;">
+		<div class="container text-center">
+			<div class="row">
+				<div class="col-sm-10 col-sm-offset-1">
+					<div class="item" data-scrollreveal="enter top over 0.4s after 0.1s">
+						<h1 class="callactiontitle"> Promote Items Area Give Discount to Buyers <span class="callactionbutton"><i class="fa fa-gift"></i> WOW24TH</span>
+						</h1>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
-</div>
+	</section>
 
 
-<!-- CALL TO ACTION =============================-->
-<section class="content-block" style="background-color:#00bba7;">
-<div class="container text-center">
-	<div class="row">
-		<div class="col-sm-10 col-sm-offset-1">
-			<div class="item" data-scrollreveal="enter top over 0.4s after 0.1s">
-				<h1 class="callactiontitle"> Promote Items Area Give Discount to Buyers <span class="callactionbutton"><i class="fa fa-gift"></i> WOW24TH</span>
-				</h1>
-			</div>
-		</div>
-	</div>
-</div>
-</section>
+	<!-- FOOTER =============================-->
+	<?php include './includes/footer.php'; ?>
 
+	<!-- SCRIPTS =============================-->
+	<script src="js/jquery-.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/anim.js"></script>
+	<script>
+		//----HOVER CAPTION---//	  
+		jQuery(document).ready(function($) {
+			$('.fadeshop').hover(
+				function() {
+					$(this).find('.captionshop').fadeIn(150);
+				},
+				function() {
+					$(this).find('.captionshop').fadeOut(150);
+				}
+			);
+		});
+	</script>
 
-<!-- FOOTER =============================-->
-<?php include './includes/footer.php';?>
-
-<!-- SCRIPTS =============================-->
-<script src="js/jquery-.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/anim.js"></script>
-<script>
-//----HOVER CAPTION---//	  
-jQuery(document).ready(function ($) {
-	$('.fadeshop').hover(
-		function(){
-			$(this).find('.captionshop').fadeIn(150);
-		},
-		function(){
-			$(this).find('.captionshop').fadeOut(150);
-		}
-	);
-});
-</script>
-	
 </body>
+
 </html>
